ea837ef4a386124159de2d8b784785e7eb1d9110
Mashood Siddiquie <mashoodsidd97@gmail.com>
2025-03-19 16:42:30 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/app/absence/manager/absence/manager/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.absence_manager.absence_manager_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:absence_manager_app/app/app.dart';
+import 'package:absence_manager_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
