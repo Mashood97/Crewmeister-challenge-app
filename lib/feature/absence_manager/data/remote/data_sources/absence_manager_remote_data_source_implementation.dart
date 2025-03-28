78907a7ba6ff8014bd4c4c1524e0488ead21ece2
Mashood Siddiquie <mashoodsidd97@gmail.com>
2025-03-28 23:20:33 +0500

    1. Fixed Api Routes and added local json files for data fetching if mock api failed to generate response.

--- lib/feature/absence_manager/data/remote/data_sources/absence_manager_remote_data_source_implementation.dart
+++ lib/feature/absence_manager/data/remote/data_sources/absence_manager_remote_data_source_implementation.dart
@@ -2,26 +2,43 @@ import 'dart:convert';
 
 import 'package:absence_manager_app/core/error/response_error.dart';
 import 'package:absence_manager_app/feature/absence_manager/data/models/response_model/absence_response_model.dart';
-import 'package:absence_manager_app/feature/absence_manager/data/models/response_model/user_response_model.dart';
-import 'package:absence_manager_app/feature/absence_manager/data/remote/data_sources/absence_manager_remote_data_source.dart';
-import 'package:absence_manager_app/feature/absence_manager/data/remote/service/absence_manager_chopper_service.dart';
-import 'package:absence_manager_app/utils/chopper_client/chopper_client.dart';
+      import 'package:absence_manager_app/feature/absence_manager/data/models/response_model/user_response_model.dart';
+      import 'package:absence_manager_app/feature/absence_manager/data/remote/data_sources/absence_manager_remote_data_source.dart';
+      import 'package:absence_manager_app/feature/absence_manager/data/remote/service/absence_manager_chopper_service.dart';
+      import 'package:absence_manager_app/utils/chopper_client/chopper_client.dart';
+      import 'package:absence_manager_app/utils/constant/app_constant.dart';
+      import 'package:absence_manager_app/utils/extensions/string_extensions.dart';
 
-class AbsenceManagerRemoteDataSourceImplementation
+    class AbsenceManagerRemoteDataSourceImplementation
     implements AbsenceManagerRemoteDataSource {
-  final AbsenceManagerChopperService absenceManagerChopperService =
-      AbsenceManagerChopperService.create(ChopperClientInstance.client);
+    final AbsenceManagerChopperService absenceManagerChopperService =
+    AbsenceManagerChopperService.create(ChopperClientInstance.client);
 
-  @override
-  Future<List<AbsenceResponseModel>> fetchAbsenceListFromServer() async {
+    @override
+    Future<List<AbsenceResponseModel>> fetchAbsenceListFromServer() async {
     final response =
-        await absenceManagerChopperService.fetchAbsenceListFromServer();
+    await absenceManagerChopperService.fetchAbsenceListFromServer();
     if (response.isSuccessful) {
-      final body = response.bodyString;
-      final decodedBody = jsonDecode(body);
-      final items = decodedBody['payload'] as List<dynamic>;
-      if (items.isNotEmpty) {
-        return items
+    final body = response.bodyString;
+    final decodedBody = jsonDecode(body);
+    final items = decodedBody['payload'] as List<dynamic>;
+    if (items.isNotEmpty) {
+    return items
+        .map(
+    (e) => AbsenceResponseModel.fromJson(
+    e as Map<String, dynamic>,
+    ),
+    )
+        .toList();
+    }
+    }
+
+    /// Mocking the local response to get the app work (Just for the test) to
+    /// fix the mock api issue.
+    else {
+      final result = await MockLocalData().absences();
+      if (result.isNotEmpty) {
+        return result
             .map(
               (e) => AbsenceResponseModel.fromJson(
                 e as Map<String, dynamic>,
@@ -33,10 +50,12 @@ class AbsenceManagerRemoteDataSourceImplementation
 
     if (response.error != null) {
       final body = response.error as String?;
+      var errorMsg = 'No Record Found';
+      if (body?.isTextNotNullAndNotEmpty == true) {
+        errorMsg = body ?? 'No Record Found';
+      }
 
-      throw ResponseError(
-        errorStatus: body ?? 'No Record Found',
-      );
+      throw ResponseError(errorStatus: errorMsg);
     }
     return [];
   }
@@ -60,12 +79,29 @@ class AbsenceManagerRemoteDataSourceImplementation
       }
     }
 
+    /// Mocking the local response to get the app work (Just for the test) to
+    /// fix the mock api issue.
+    else {
+      final result = await MockLocalData().members();
+      if (result.isNotEmpty) {
+        return result
+            .map(
+              (e) => UserResponseModel.fromJson(
+                e as Map<String, dynamic>,
+              ),
+            )
+            .toList();
+      }
+    }
+
     if (response.error != null) {
       final body = response.error as String?;
+      var errorMsg = 'No Record Found';
+      if (body?.isTextNotNullAndNotEmpty == true) {
+        errorMsg = body ?? 'No Record Found';
+      }
 
-      throw ResponseError(
-        errorStatus: body ?? 'No Record Found',
-      );
+      throw ResponseError(errorStatus: errorMsg);
     }
     return [];
   }
--- lib/utils/constant/app_constant.dart
+++ lib/utils/constant/app_constant.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:absence_manager_app/core/platform/platform_helper.dart';
 import 'package:absence_manager_app/feature/absence_manager/domain/entities/response_entity/absence_response_entity.dart';
+import 'package:flutter/services.dart'; // Use rootBundle for web compatibility
 
 class AppConstant {
   factory AppConstant() {
@@ -27,4 +30,35 @@ class AppConstant {
   String _formatDate(DateTime date) {
     return '${date.toUtc().toIso8601String().replaceAll('-', '').replaceAll(':', '').split('.')[0]}Z';
   }
+}
+
+class MockLocalData {
+  factory MockLocalData() {
+    return _instance;
+  }
+
+  MockLocalData._internal();
+
+  static final MockLocalData _instance = MockLocalData._internal();
+
+  /// Path to json files in assets folder.
+
+  static const String _jsonDir = 'assets/json';
+  static const String absencesPath = '$_jsonDir/absences.json';
+  static const String membersPath = '$_jsonDir/members.json';
+
+  Future<List<dynamic>> _readJsonFile(String path) async {
+    try {
+      final content = await rootBundle.loadString(path);
+      final data = jsonDecode(content) as Map<String, dynamic>;
+      final items = data['payload'] as List<dynamic>?;
+      return items ?? []; // Ensure a default empty list
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<List<dynamic>> absences() async => _readJsonFile(absencesPath);
+
+  Future<List<dynamic>> members() async => _readJsonFile(membersPath);
 }
